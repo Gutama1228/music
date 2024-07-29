d8fb2a623c692484e222976afdfbfcce7e059d14
gutama <142733438+Gutama1228@users.noreply.github.com>
2024-07-29 17:44:08 +0700

    update config/config.py

--- config/config.py
+++ config/config.py
@@ -55,7 +55,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/TheTeamVivek/ExtraPlugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
